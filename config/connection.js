60696e47e92703180f96f00ad6a6f75d77b50c8e
sharklemon <anastasia.l.mark@gmail.com>
2017-09-06 23:01:00 -0400

    more heroku with jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port:  process.env.PORT || 3000,
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "meds_db"
-});
+//heroku stuff
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection=mysql.createConnection({
+    port:  process.env.PORT || 3000,
+    host:'localhost',
+    user:'root',
+    password:'12345'
+    database:"meds_db"
+  })
+}
+
 
 // Make connection.
 connection.connect(function(err) {
